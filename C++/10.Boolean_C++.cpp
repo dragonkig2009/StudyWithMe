96525dec554cd0741c40c1064c9134429251649a
Tên đúng <Email ng>
2025-07-10 14:52:57 +0700

    Hi có ai đó xem cái xà lơ này của tôi không -.- update thêm bài 9 và 10 z thui, anh em mình cứ z thui hẹhehe

---
+++ C++/10.Boolean_C++.cpp
@@ -0,0 +1,9 @@
+/*
+I. Boolean Value
+1. kiểu  dữ liệu boolean đc khai báo theo kiểu: bool x = true/false
+2. khi return giá trị được trả về sẽ là true = 1 - false = 0
+II. Boolean Experesion
+1. sự dụng các operato <,>,==,... trong cout
+
+
+*/
